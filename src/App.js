54fb2371de863ddecc11204e3645d8f3c1dfcbe1
vignesh <vigneshreddyjulakanti@gmail.com>
2023-11-23 14:21:57 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ScormSupport from './Components/ScormSupport';
+
+function App() {
+  return (
+    <div className="App">
+    <ScormSupport/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ScormSupport.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function ScormSupport() {
+  return (
+    <div>
+    <div>ScormSupport</div>
+   
+
+    <iframe 
+        title="SCORM Content"
+        src="http://localhost:3000/scormFiles/story.html"
+        width="800"
+        height="600"
+        allowFullScreen 
+      ></iframe>
+   
+    </div>
+  )
+}
